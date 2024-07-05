b5c911820043377c769aa12d25558f3621ac3829
Jane Doe <johanluna777@gmail.com>
2024-07-05 00:09:48 -0500

    Upload complete project

---
+++ antivirus_s3/antivirus_s3/views.py
@@ -0,0 +1,18 @@
+# Creando nuestra primera vista
+
+from django.http import HttpResponse
+
+
+def analyze_files(file):
+    print(f'Scanning file = {file}')
+    return HttpResponse(f'Archivo {file} Analizado')
+
+
+def analyze_bucket(bucket):
+    print(f'Scanning Bucket = {bucket}')
+    return HttpResponse(f'Bucket {bucket} Analizado')
+
+
+def reports(request):
+    print(f'Reportes Generados')
+    return HttpResponse('Reportes Generados')
